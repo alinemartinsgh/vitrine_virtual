67ea755a46cea2bbe41cca20b0df8054921abb9d
alinemartinsgh <aline.p.martins@globalhitss.com.br>
2021-09-20 14:13:26 -0300

    config completa

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renderizar componente App com texto vitrine virtual', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const texto = screen.getByText(/vitrine virtual/i);
+  expect(texto).toBeInTheDocument();
 });
