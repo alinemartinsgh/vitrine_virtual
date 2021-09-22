53c93a26800511b8cdff63fbb9f9966c676d0bb0
alinemartinsgh <aline.p.martins@globalhitss.com.br>
2021-09-22 18:08:20 -0300

    adição do mocklistaObjetosS3 no teste de listar imagens

---
+++ server/src/storage/__mocks__/index.ts
@@ -0,0 +1,37 @@
+export const mockListaObjetosS3 = [
+  {
+    Key: '563bb74e-c97e-40f3-b70a-344262932482-flor.jpg',
+    LastModified: '2021-09-22T18:34:09.000Z',
+    ETag: '"bfff425d96c0bc53c0bc69d0836945cf"',
+    Size: 338815,
+    StorageClass: 'STANDARD',
+  },
+  {
+    Key: '563bb74e-c97e-40f3-b70a-344262932482-flor.jpg',
+    LastModified: '2021-09-22T18:34:09.000Z',
+    ETag: '"bfff425d96c0bc53c0bc69d0836945cf"',
+    Size: 338815,
+    StorageClass: 'STANDARD',
+  },
+  {
+    Key: '563bb74e-c97e-40f3-b70a-344262932482-flor.jpg',
+    LastModified: '2021-09-22T18:34:09.000Z',
+    ETag: '"bfff425d96c0bc53c0bc69d0836945cf"',
+    Size: 338815,
+    StorageClass: 'STANDARD',
+  },
+  {
+    Key: '563bb74e-c97e-40f3-b70a-344262932482-flor.jpg',
+    LastModified: '2021-09-22T18:34:09.000Z',
+    ETag: '"bfff425d96c0bc53c0bc69d0836945cf"',
+    Size: 338815,
+    StorageClass: 'STANDARD',
+  },
+  {
+    Key: '563bb74e-c97e-40f3-b70a-344262932482-flor.jpg',
+    LastModified: '2021-09-22T18:34:09.000Z',
+    ETag: '"bfff425d96c0bc53c0bc69d0836945cf"',
+    Size: 338815,
+    StorageClass: 'STANDARD',
+  },
+];
--- server/src/storage/__tests__/imagem.test.ts
+++ server/src/storage/__tests__/imagem.test.ts
@@ -1,5 +1,6 @@
 import { s3 } from '../config';
 import { listaImagens, deletaImagem } from '../imagem';
+import { mockListaObjetosS3 } from '../__mocks__';
 
 describe('storage-imagem', () => {
   const promise = jest.fn();
@@ -7,14 +8,14 @@ describe('storage-imagem', () => {
     const listObjectsV2Spy = jest.spyOn(s3, 'listObjectsV2');
     it('deve retornar um array de objetos que estão no bucket', async () => {
       listObjectsV2Spy.mockReturnValue({
-        promise: promise.mockResolvedValue('lista de objetos'),
+        promise: promise.mockResolvedValue(mockListaObjetosS3),
       } as never);
       const response = await listaImagens();
       expect(listObjectsV2Spy).toHaveBeenCalledWith({
         Bucket: 'bucketvitrinevirtual',
       });
-      expect(promise).toHaveBeenCalledWith();
-      expect(response).toEqual('lista de objetos');
+      expect(promise).toBeCalledWith();
+      expect(response).toEqual(mockListaObjetosS3);
     });
   });
 
