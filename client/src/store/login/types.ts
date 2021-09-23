312e583a0d1acb512f4d062c126433b3d5783863
LeoAntunesBrombilla <antunes.b.leonardo@gmail.com>
2021-09-23 16:42:27 -0300

    tipos de login adicionados

--- client/src/store/login/types.ts
+++ client/src/store/login/types.ts
@@ -1,1 +1,28 @@
-export {}
+export enum LoginTypes {
+  LOGIN = '@login/LOGIN',
+  REQUEST_EMAIL_PASSWORD = '@login/REQUEST_EMAIL_PASSWORD',
+  LOGOUT = '@login/LOGOUT',
+  LOGIN_ERROR = '@login/LOGIN_ERROR',
+}
+
+export type LoginState = {
+  isLogged: boolean;
+  isLoading: boolean;
+  email: string;
+  error?: Error;
+};
+
+export type LoginAction<Payload> = {
+  type: LoginTypes;
+  payload: Payload;
+};
+
+export type LoginReducer = (
+  state: LoginState,
+  actions: LoginAction<LoginState>,
+) => LoginState;
+
+export interface LoginForm {
+  email: string;
+  password: string;
+}
