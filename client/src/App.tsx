2b258d05894e5d1107247dc4e4f7d7d90f0c5f35
LeoAntunesBrombilla <antunes.b.leonardo@gmail.com>
2021-10-08 07:11:35 -0300

    id e campanha indo para pagina de editar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import CampanhaPage from './pages/CampanhaPage';
+import EditarPage from './pages/EditarPage';
 
 const { store } = configureStore();
 
@@ -16,6 +17,7 @@ export default function App() {
           <Route exact path="/" component={LoginPage} />
           <Route exact path="/homePage" component={HomePage} />
           <Route exact path="/novaCampanha" component={CampanhaPage} />
+          <Route exact path="/editar" component={EditarPage} />
         </Switch>
       </BrowserRouter>
     </Provider>
--- client/src/pages/HomePage/index.tsx
+++ client/src/pages/HomePage/index.tsx
@@ -1,22 +1,14 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { actions } from 'src/store/campanhas';
-import { getListaCampanhas } from 'src/store/campanhas/selectors';
+
+import ListaCampanha from '../../components/listaCampanha';
 
 interface HomePageProps {}
 
 const HomePage: React.FC<HomePageProps> = () => {
-  const dispatch = useDispatch();
-  const listaCampanhas = useSelector(getListaCampanhas);
-
-  console.log(listaCampanhas);
-  function chamaLista() {
-    dispatch(actions.buscaListaCampanhas());
-  }
   return (
     <div>
       <h1>HomePage</h1>
-      <button onClick={chamaLista}>lista</button>
+      <ListaCampanha />
     </div>
   );
 };
